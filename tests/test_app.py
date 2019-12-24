9d1fa40b6858dc8daa6bf0c81987f28b7a0f1340
Jose P. Garcia <josepgl@gmail.com>
2019-12-25 03:20:46 +0700

    Import app factory in test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,12 @@
 import unittest
 
-from app import app
+from app import create_app
 
 
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
+        app = create_app()
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
