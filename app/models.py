e6a19a9e211879875f109b5487b27e01259ea5a4
Jose P. Garcia <josepgl@gmail.com>
2019-12-30 19:13:17 +0700

    Partial implementation of models for SQLAlchemy

--- app/models.py
+++ app/models.py
@@ -1,8 +1,63 @@
 from . import db
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Boolean, PickleType
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-class Engine(db.Model):
+Base = declarative_base()
 
-    # Columns
+
+# class Employer(db.Model):
+class Employer(Base):
+    __tablename__ = 'employer'
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(128))
-    thrust = db.Column(db.Integer, default=0)
+    code = db.Column(db.String(128), nullable=False, unique=True)
+    name = db.Column(db.String(128), nullable=False, unique=True)
+    url = db.Column(db.String(128), nullable=False, unique=True)
+    logo = db.Column(db.String(128), nullable=False, unique=True)
+    location = db.Column(db.String(128), nullable=False, unique=True)
+    industry = db.Column(db.String(128), nullable=False, unique=True)
+    employees = db.Column(db.String(128), nullable=False, unique=True)
+    country = db.Column(db.String(128), nullable=False, unique=True)
+    working_days = db.Column(db.String(128), nullable=False, unique=True)
+    overtime = db.Column(db.String(128))
+    website = db.Column(db.String(128))
+
+    description = db.Column(db.String(128), nullable=False, unique=True)
+    tags = db.Column(db.String(128), db.ForeignKey('tag.name'), nullable=False)
+
+    jobs = db.relationship('Job', backref='employer', lazy=True)
+
+    def __repr__(self):
+        return '<Employer {}>'.format(self.code)
+
+
+# class Job(db.Model):
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    last_update = db.Column(db.String(128), nullable=False)
+    title = db.Column(db.String(128), nullable=False)
+    url = db.Column(db.String(128), nullable=False)
+    salary = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(128), nullable=False)
+    address_1 = db.Column(db.String(128), nullable=False)
+    address_2 = db.Column(db.String(128), nullable=False)
+
+    employer_code = db.Column(db.String(128), db.ForeignKey('employer.code'), nullable=False)
+    tags = db.Column(db.String(128), db.ForeignKey('tag.name'), nullable=False)
+
+    def __repr__(self):
+        return '<Job {}>'.format(self.id)
+
+
+# class Tag(db.Model):
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), unique=True)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
