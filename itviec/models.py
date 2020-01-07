8a85a77c06d543babf81c874fb1fda9e2d5dd240
Jose P. Garcia <josepgl@gmail.com>
2020-01-07 18:44:31 +0700

    Reworked models based on SQLAlchemy ORM

--- itviec/models.py
+++ itviec/models.py
@@ -1,14 +1,32 @@
-from sqlalchemy import Column, Table, Integer, String, ForeignKey, Text, DateTime, Float, Boolean, PickleType
+from sqlalchemy import Column, Table, ForeignKey, UniqueConstraint
+from sqlalchemy import Integer, String, Text, Boolean
 from sqlalchemy.orm import relationship, backref
 
-from itviec.db import Base, db_session
+import config
+from itviec.db import Base, session
 from itviec.helpers import fetch_url
-from itviec.parsers import parse_employer, parse_employer_review, parse_job_summary
-import config as Config
-# from pprint import pprint
+from itviec.parsers import JobSummaryParser, parse_employer, parse_employer_review
+from pprint import pprint
+
+job_address = Table('job_address', Base.metadata,
+                    Column('job_id', Integer, ForeignKey('job.id')),
+                    Column('address_id', Integer, ForeignKey('address.id'))
+                    )
 
 
-# class Employer(db.Model):
+# job_tag = Table('job_tag', Base.metadata,
+#                 Column('job_id', Integer, ForeignKey('job.id')),
+#                 Column('tag_id', Integer, ForeignKey('tag.id')),
+
+#                 # UniqueConstraint('job_id', 'tag_id')
+#                 )
+
+# employer_job = Table('employer_job', Base.metadata,
+#                      Column('employer_id', Integer, ForeignKey('employer.id')),
+#                      Column('job_id', Integer, ForeignKey('job.id')),
+#                      )
+
+
 class Employer(Base):
     __tablename__ = 'employer'
 
@@ -24,44 +42,35 @@ class Employer(Base):
     overtime = Column(String(128))
     website = Column(String(128))
 
-    description = Column(Text(), nullable=False, unique=True)  # Large text
-    tags = Column(String(128), ForeignKey('tag.name'), nullable=False)
-
     # Description fields
+    description = Column(Text(), nullable=False, unique=True)  # Large text
     panel = Column(Text(), nullable=False, unique=True)
     # header = Column(String(), nullable=False, unique=True)
 
-    # job_ids = Column(Integer, ForeignKey('job.id'), nullable=False)
-    # job_ids = relationship('Job.id', backref='employer', lazy=True)
-    # job_ids = relationship('Job', lazy=True)
-    # job_ids = relationship("Employer",
-    # job_ids = relationship("employer_jobids",
-    #                        secondary="employer_jobids",
-    #                        backref=backref("employer", lazy='dynamic')
-    #                        )
     # jobs = relationship('Job', lazy=True)
-
-    reviews = relationship('Review', backref='employer', lazy=True)
+    # tags = Column(String(128), ForeignKey('tag.name'), nullable=False)
+    # reviews = relationship('Review', backref='employer', lazy=True)
 
     def __repr__(self):
         return '<Employer {}: {}>'.format(self.code, self.name)
 
     def url(self):
-        return Config.TEMPLATE_EMPLOYER_URL.format(self.code)
+        return config.Config.TEMPLATE_EMPLOYER_URL.format(self.code)
 
     @classmethod
     def request_employer(self, code):
-        employer_url = Config.TEMPLATE_EMPLOYER_URL.format(code)
+        employer_url = config.Config.TEMPLATE_EMPLOYER_URL.format(code)
         # print(employer_url)
         employer_response = fetch_url(employer_url)
         employer_dict = parse_employer(employer_response.text, code)
         employer_dict['panel'] = "<div>"
-        print(employer_dict)
+        # print(employer_dict)
 
         # print("#### Review ##################")
-        review_url = Config.TEMPLATE_EMPLOYER_REVIEW_URL.format(code)
+        review_url = config.Config.TEMPLATE_EMPLOYER_REVIEW_URL.format(code)
         review_response = fetch_url(review_url)
         review_dict = parse_employer_review(review_response.text)
+        pprint(review_dict['reviews'][0])
         # print(review_dict)
 
         reviews = []
@@ -99,116 +108,156 @@ class Job(Base):
     description = Column(String(128), nullable=False)
 
     address = relationship("Address",
-                           secondary="job_addresses",
-                           backref=backref("job", lazy='dynamic')
+                           secondary="job_address",
+                           backref="job",
+                           # backref=backref("jobs", lazy='dynamic'),
                            )
 
-    tags = relationship("Tag",
-                        secondary="job_tags",
-                        backref=backref("job", lazy='dynamic')
-                        )
+    # tags = relationship("Tag",
+    #                     secondary="job_tag",
+    #                     backref="job",
+    #                     backref=backref("jobs", lazy='dynamic'),
+    #                     back_populates="jobs",
+    #                     )
 
-    employer_code = Column(String(128), nullable=False)
-    # employer = relationship('Employer', backref='jobs', load_on_pending=True)
-    # employer = relationship('Employer', backref='jobs')
+    # employer_code = Column(String(128), nullable=False)
+    employer_code = Column(String(128), ForeignKey('employer.code'), nullable=False)
+    employer = relationship("Employer", backref=backref("jobs", order_by=id))
     # employer = relationship("Employer",
-    #                         # secondary="employer_jobs",
-    #                         backref=backref("jobs", lazy='dynamic')
+    #                         secondary="employer_job",
+    #                         backref="jobs",
     #                         )
 
     def __repr__(self):
         return '<Job {}: {}>'.format(self.id, self.title)
 
+    def save(self):
+        session.add(self)
+        session.commit()
+
     @classmethod
     def from_tag(self, tag):
-        # job_url = Config.TEMPLATE_EMPLOYER_URL.format(jid)
         # job_response = fetch_url(job_url)
-        job_dict = parse_job_summary(tag)
-        # print(job_dict)
+        job_p = JobSummaryParser(tag)
+        job_dict = job_p.get_dict()
+        return self.from_dict(job_dict)
 
-        has_job = db_session.query(Job).filter_by(id=job_dict["id"]).first()
-        if has_job:
-            return has_job
+    @classmethod
+    def from_dict(self, job_dict):
+        # has_job = session.query(Job).filter_by(id=job_dict["id"]).first()
+        # if has_job:
+        #     return has_job
 
         # Child objects
         addresses = []
         for job_address in job_dict["address"]:
             # print("job_address: " + job_address)
-
-            has_addr = db_session.query(Address).filter_by(name=job_address).first()
+            has_addr = session.query(Address).filter_by(name=job_address).first()
             if has_addr is not None:
                 addresses.append(has_addr)
             else:
                 addresses.append(Address(name=job_address))
-        job_dict["address"] = addresses
+                session.add(addresses[-1])
+            # pprint(addresses[-1].__dict__)
 
         tags = []
         for job_tag in job_dict["tags"]:
             # print("job_tag: " + job_tag)
-            has_tag = db_session.query(Tag).filter_by(name=job_tag).first()
+            has_tag = session.query(Tag).filter_by(name=job_tag).first()
             if has_tag is not None:
                 tags.append(has_tag)
             else:
                 tags.append(Tag(name=job_tag))
-        job_dict["tags"] = tags
+                session.add(tags[-1])
+            # pprint(tags[-1].__dict__)
 
-        # print(job_dict)
+        session.commit()
+        job_dict["address"] = []
+        job_dict["tags"] = []
         job = Job(**job_dict)
-
-        db_session.add(job)
-        db_session.commit()
+        job.address.extend(addresses)
+        for tag in tags:
+            job.link_tag(tag)
 
         return job
+
+    def link_tag(self, tag):
+        has_link = session.query(JobTag).filter_by(job_id=self.id, tag_id=tag.id).first()
+
+        if has_link:
+            print("{} already exists".format(has_link))
+            return None
+
+        job_tag_link = JobTag(job_id=self.id, tag_id=tag.id)
+        print("Created: {}".format(job_tag_link))
+        session.add(job_tag_link)
+
+        return job_tag_link
+
+
+class JobTag(Base):
+    __tablename__ = 'job_tag'
+
+    job_tag_id = Column(Integer(), primary_key=True)
+    job_id = Column(Integer(), ForeignKey('job.id'))
+    tag_id = Column(Integer(), ForeignKey('tag.id'))
+
+    job = relationship("Job", backref=backref("tags"))
+    tag = relationship("Tag", uselist=False)
+
+    __table_args__ = (UniqueConstraint('job_id', 'tag_id'),)
+
+    def __repr__(self):
+        return '<JobTag job={} tag={}>'.format(self.job_id, self.tag_id)
 
 
 class Tag(Base):
     __tablename__ = 'tag'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String(128), unique=True, nullable=False)
+
+    # jobs = relationship("Job",
+    #                     secondary="job_tag",
+    #                     # backref="job",
+    #                     # backref=backref("jobs", lazy='dynamic'),
+    #                     back_populates="tags",
+    #                     )
+    # jobs = relationship("JobTag")
+    # jobs = relationship("JobTag", secondary="job_tag")
+    # employers = relationship("Employer", secondary="employer_tag")
 
     def __repr__(self):
         return '<Tag {}>'.format(self.name)
 
 
-class Review(Base):
-    __tablename__ = 'review'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    title = Column(String(128), unique=True)
-    date = Column(String(128), unique=True)
-    employer_code = Column(String(128), ForeignKey('employer.code'), nullable=False)
-
-    def __repr__(self):
-        return '<Review {}>'.format(self.title)
-
-
 class Address(Base):
     __tablename__ = 'address'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Address {}>'.format(self.name)
 
 
-job_addresses = Table('job_addresses',
-                      Base.metadata,
-                      Column('job_id', Integer, ForeignKey('job.id')),
-                      Column('address_id', Integer, ForeignKey('address.id'))
-                      )
+class Review(Base):
+    __tablename__ = 'review'
 
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    title = Column(String(128), unique=True)
+    date = Column(String(128), unique=True)
+    employer_code = Column(String(128), ForeignKey('employer.code'), nullable=False)
+    last_update = Column(String(128))
+    liked = Column(Text())
+    hated = Column(Text())
+    recommend = Column(Boolean(), nullable=False)
+    stars_total = Column(Integer(), nullable=False)
+    stars_salary = Column(Integer(), nullable=False)
+    stars_training = Column(Integer(), nullable=False)
+    stars_management = Column(Integer(), nullable=False)
+    stars_culture = Column(Integer(), nullable=False)
+    stars_workspace = Column(Integer(), nullable=False)
 
-job_tags = Table('job_tags',
-                 Base.metadata,
-                 Column('job_id', Integer, ForeignKey('job.id')),
-                 Column('tag_id', Integer, ForeignKey('tag.id'))
-                 )
-
-
-# employer_jobids = Table('employer_jobids',
-#                         Base.metadata,
-#                         Column('employer_id', Integer, ForeignKey('employer.id')),
-#                         Column('job_id', Integer, ForeignKey('job.id')),
-#                         )
+    def __repr__(self):
+        return '<Review {}>'.format(self.title)
