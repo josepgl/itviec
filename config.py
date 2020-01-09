04de6c9dd0da3e651857ef942a8c2d8d4848be60
Jose P. Garcia <josepgl@gmail.com>
2020-01-09 14:40:11 +0700

    Rename load_config to init_app like other flask modules, loads config from profile name parameter

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
 import os
 
+'''
+- Environment config
+  - Instance config or
+  - Test config
+'''
 
-# def print_json(to_json):
-#     import json
-#     json = json.dumps(to_json, indent=4, sort_keys=True, default=str)
-#     print(json)
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+
+req_http_headers = dict()
+
+
+# Function for debugging, avoiding circular dependency with helpers module
+def print_json(to_json_string):
+    import json
+    json = json.dumps(to_json_string, indent=4, sort_keys=True, default=str)
+    print(json)
 
 
 class Config:
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+    INSTANCE_DIR = os.path.join(basedir, "instance")
 
     CONFIG_FILENAME = "config.py"
 
@@ -21,81 +32,69 @@ class Config:
     TEMPLATE_EMPLOYER_REVIEW_URL = "https://itviec.com/companies/{}/review"
 
     # Database / SQLAlchemy
-    # DATABASE_FILENAME = "ItViec.sqlite"
-    DATABASE_FILENAME = "sqlalchemy.sqlite"
-    DATABASE = os.path.join(INSTANCE_DIR, DATABASE_FILENAME)
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-        'sqlite:///' + os.path.join(INSTANCE_DIR, DATABASE_FILENAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
-
-    # Bootstrap
     BOOTSTRAP_SERVE_LOCAL = True
 
-    # ItViec json header
+    # ItViec request header for json
     HTTP_HEADER_X_REQUESTED_WITH = "XMLHttpRequest"
     # HTTP_HEADER_COOKIE = "_ITViec_session=..."
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(Config.INSTANCE_DIR, 'sqlalchemy.sqlite')
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, "instance", "sqlalchemy.sqlite")
+    SQLALCHEMY_ECHO = True
 
 
 class TestingConfig(Config):
-    DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "sqlite://"
+    SQLALCHEMY_ECHO = True
 
 
 class ProductionConfig(Config):
-    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+        'sqlite:///' + os.path.join(basedir, "instance", "sqlalchemy.sqlite")
 
 
-config_by_name = dict(
+config = dict(
     development=DevelopmentConfig,
-    test=TestingConfig,
-    production=ProductionConfig
-)
+    testing=TestingConfig,
+    production=ProductionConfig,
 
-req_http_headers = dict()
+    default=DevelopmentConfig,
+)
 
 
 # Uses flask.Config (app.config)
-def load_config(config, test_config=None):
+def init_app(app, profile=None, test_config=None):
     global req_http_headers
 
     # load the instance config, if it exists, when not testing
-    config_name = os.getenv("FLASK_ENV", "production")
-    print("Loading {} configuration profile.".format(config_name))
+    config_name = profile or os.getenv("FLASK_ENV", "production")
+    print("Loading '{}' configuration profile".format(config_name))
 
     # load default config
-    config.from_object(config_by_name[config_name]())
+    app.config.from_object(config[config_name]())
 
-    if test_config is None:
-        try:
-            # load customized config
-            print("Loading custom configuration.")
-            config.from_pyfile("config.py")
-            # app.config.from_pyfile('config.py', silent=True)
-        except FileNotFoundError:
-            print("Custom config.py file missing in instance folder.")
-            pass
+    if test_config:
+        print("Loading test configuration.")
+        app.config.from_mapping(test_config)
     else:
-        # load the test config if passed in
-        config.from_mapping(test_config)
+        try:
+            print("Loading instance configuration.")
+            app.config.from_pyfile("config.py")
+        except FileNotFoundError as error:
+            print("Custom config.py file missing in instance folder: {}".format(error))
 
-    req_http_headers = collect_http_headers(config)
+    req_http_headers = collect_http_headers(app.config)
 
-    # Debugging
-    # print_json(config)
+    if app.config["DEBUG"] is True:
+        print_json(app.config)
 
 
 def collect_http_headers(conf):
     for k, v in conf.get_namespace('HTTP_HEADER_').items():
         req_http_headers[k.replace("_", "-").capitalize()] = v
     return req_http_headers
-
-
-# req_http_headers = collect_http_headers(conf)
