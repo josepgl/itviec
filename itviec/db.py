c753c818b5bb622fbb3ad34b77a7d8f0e1a3cd7f
Jose P. Garcia <josepgl@gmail.com>
2020-01-09 16:10:01 +0700

    Make a class with database logic

--- itviec/db.py
+++ itviec/db.py
@@ -2,24 +2,51 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-import config
 
-engine = create_engine(config.Config.SQLALCHEMY_DATABASE_URI,
-                       convert_unicode=True,
-                       # echo=True
-                       )
+class Database():
 
-session = scoped_session(sessionmaker(autocommit=False,
-                                      autoflush=False,
-                                      bind=engine))
+    def __init__(self, uri=None):
+        self.base = declarative_base()
+        self.session = None
 
-Base = declarative_base()
-Base.query = session.query_property()
+        if uri is not None:
+            self.set_engine(uri)
+
+    def set_uri(self, uri):
+        self.engine = create_engine(uri)
+        # print("Engine: " + str(self.engine))
+        print(self.engine)
+        session = self.get_session()
+        self.base.query = session.query_property()
+
+    def get_session(self):
+        if self.session is None:
+            self.session = scoped_session(
+                                sessionmaker(autocommit=False,
+                                             autoflush=False,
+                                             bind=self.engine))
+
+        return self.session
+
+    def init_app(self, app):
+        uri = app.config["SQLALCHEMY_DATABASE_URI"]
+        self.set_uri(uri)
+
+        print("Enabling close session on app context teardown")
+        @app.teardown_appcontext
+        def close_session(exception=None):
+            db.session.remove()
+
+        if uri is 'sqlite://':
+            self.init_app()
+
+    def init_db(self):
+        print("Init database")
+        # import all modules here that might define models so that
+        # they will be registered properly on the metadata.  Otherwise
+        # you will have to import them first before calling init_db()
+        import itviec.models
+        self.base.metadata.create_all(bind=self.engine)
 
 
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import itviec.models
-    Base.metadata.create_all(bind=engine)
+db = Database()
